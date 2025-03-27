90739551ac287d8dcdde00484f882fbee10bc98e
Lu <luhui19970305@hotmail.com>
2025-03-27 03:48:16 +0000

    apply configs

--- src/handlers/shortlink.rs
+++ src/handlers/shortlink.rs
@@ -8,23 +8,22 @@ use axum::{Json, http::StatusCode};
 use base62;
 use chrono;
 use diesel::prelude::*;
-use diesel_async::pooled_connection::bb8::Pool;
-use diesel_async::{AsyncPgConnection, RunQueryDsl};
+use diesel_async::RunQueryDsl;
 // use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
 
-use crate::models;
 use crate::schema;
-
-pub fn generate_shortlink(url: &str, identifier: Option<&str>) -> String {
+use crate::{models, state::AppState};
+pub fn generate_shortlink(url: &str, identifier: Option<&str>, length: Option<usize>) -> String {
+    let length = length.unwrap_or(7).min(16); // Default to 7 and cap at 16
     let target = match identifier {
         Some(identifier) => &(url.to_string() + identifier),
         None => url,
     };
     let hash = Sha256::digest(target.as_bytes());
     let number = u128::from_le_bytes(hash[..16].try_into().unwrap());
-    return base62::encode(number)[..7].to_string();
+    return base62::encode(number)[..length].to_string();
 }
 
 // lazy_static! {
@@ -43,15 +42,21 @@ pub struct ShortlinkParams {
 }
 
 pub async fn create_shortlink(
-    State(pool): State<Pool<AsyncPgConnection>>,
+    // State(pool): State<Pool<AsyncPgConnection>>,
+    // State(config): State<Config>,
+    State(state): State<AppState>,
     Json(payload): Json<ShortlinkParams>,
 ) -> Result<Json<ShortlinkResponse>, (StatusCode, String)> {
     let now = chrono::Utc::now();
-    let mut short_hash = generate_shortlink(&payload.url, Some(&now.to_string())) + "A";
+    let mut short_hash = generate_shortlink(
+        &payload.url,
+        Some(&now.to_string()),
+        Some(state.config.shortlink_length - 1),
+    ) + "A";
 
-    let mut conn = pool.get().await.map_err(internal_error)?;
+    let mut conn = state.pool.get().await.map_err(internal_error)?;
 
-    for hash_conflicts in 0..5 {
+    for hash_conflicts in 0..state.config.shortlink_max_hash_retries {
         // TODO: check with cache first then persist
         let shortlink = schema::shortlink::table
             .filter(schema::shortlink::hash.eq(&short_hash))
@@ -59,8 +64,6 @@ pub async fn create_shortlink(
             .await
             .optional()
             .map_err(internal_error)?;
-
-        println!("{:?}", shortlink);
 
         match shortlink {
             Some(_) => {
@@ -81,7 +84,8 @@ pub async fn create_shortlink(
         .values(models::NewShortlink {
             hash: &short_hash,
             url: &payload.url,
-            expire_at: &(now + chrono::TimeDelta::days(10)).naive_utc(),
+            expire_at: &(now + chrono::TimeDelta::days(state.config.shortlink_expire_days as i64))
+                .naive_utc(),
         })
         .returning(models::ShortLink::as_returning())
         .get_result::<models::ShortLink>(&mut conn)
@@ -97,16 +101,16 @@ pub async fn create_shortlink(
     // } // Drop the MutexGuard here before await
 
     Ok(Json(ShortlinkResponse {
-        shortlink: format!("http://localhost:8080/{}", shortlink.hash),
+        shortlink: format!("{}/{}", state.config.shortlink_base_url, shortlink.hash),
         expire_at: shortlink.expire_at.to_string(),
     }))
 }
 
 pub async fn redirect_shortlink(
-    State(pool): State<Pool<AsyncPgConnection>>,
+    State(state): State<AppState>,
     Path(short_hash): Path<String>,
 ) -> Result<Redirect, (StatusCode, String)> {
-    let mut conn = pool.get().await.map_err(internal_error)?;
+    let mut conn = state.pool.get().await.map_err(internal_error)?;
 
     // TODO: replace to check cache first
     // let store = MEM_URL_STORE.lock().unwrap();
